e87dec221f42bb4781ee03997231bba9d22371c6
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2024-06-04 20:38:33 +0200

    Fix Go To slide functionality

--- src/main/java/jabberpoint/controllers/command/GoToSlideCommand.java
+++ src/main/java/jabberpoint/controllers/command/GoToSlideCommand.java
@@ -1,21 +1,36 @@
 package jabberpoint.controllers.command;
 
 import jabberpoint.Presentation;
+import jabberpoint.slideViewer.SlideViewerComponent;
 
 import javax.swing.*;
 
-public class GoToSlideCommand extends Command
-{
-    public GoToSlideCommand(Presentation presentation)
-    {
+public class GoToSlideCommand extends Command {
+
+    public GoToSlideCommand(Presentation presentation) {
         super(presentation);
     }
 
     @Override
-    public void execute()
-    {
+    public void execute() {
+        SlideViewerComponent parent = this.presentation.getSlideViewComponent();
         String pageNumberStr = JOptionPane.showInputDialog((Object) "Page number?");
-        int pageNumber = Integer.parseInt(pageNumberStr);
-        presentation.setSlideNumber(pageNumber - 1);
+        int pageNumber = 0;
+
+        try {
+            pageNumber = Integer.parseInt(pageNumberStr);
+        } catch (NumberFormatException  e) {
+            JOptionPane.showMessageDialog(
+                    parent, "A number must be provided");
+            return;
+        }
+
+        if (pageNumber > this.presentation.getSize() || pageNumber <= 0) {
+            JOptionPane.showMessageDialog(
+                    parent, "Provide a valid page number");
+            return;
+        }
+
+        this.presentation.setSlideNumber(pageNumber - 1);
     }
 }
